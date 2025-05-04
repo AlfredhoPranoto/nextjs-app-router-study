654d413717499b323a14f526fbc152a3c47fed2b
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-05-04 21:41:33 +0700

    fix: return correct HTTP status code in JsonResponse
    - Previously, JsonResponse did not include the appropriate HTTP status code for success or error cases. This update ensures proper status codes are returned.

--- src/app/util/response.ts
+++ src/app/util/response.ts
@@ -1,4 +1,4 @@
 import { NextResponse } from "next/server";
 
 export const jsonResponse = (status: number, message: string, data?: object) =>
-  NextResponse.json({ status, message, data });
+  NextResponse.json({ status, message, data }, { status });
